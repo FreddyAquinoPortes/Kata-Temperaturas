3a90fe33ca638330c2ad34c8be228b2a045abefe
FreddyAquinoPortes <72163059+FreddyAquinoPortes@users.noreply.github.com>
2023-01-12 19:03:56 -0400

    0.01

---
+++ Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Temperaturas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string Escala1="c";
+            string Escala2 = "k";
+            Console.WriteLine("Ingrese la primer temperatura:");
+            double temp1Value = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la escala de la primer temperatura (K, C, F):");
+             Escala1=Console.ReadLine();
+            Escala1 = Escala1.ToUpper();
+            Temperature.TemperatureScale temp1Scale = (Temperature.TemperatureScale)Enum.Parse(typeof(Temperature.TemperatureScale), Escala1, true);
+            
+            Temperature temp1 = new Temperature(temp1Value, temp1Scale);
+           
+            Console.WriteLine("Ingrese la segunda temperatura:");
+            double temp2Value = double.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese la escala de la segunda temperatura (K, C, F):");
+            Escala2 = Console.ReadLine();
+            Escala2 = Escala2.ToUpper();
+
+            
+            Temperature.TemperatureScale temp2Scale = (Temperature.TemperatureScale)Enum.Parse(typeof(Temperature.TemperatureScale), Escala2, true);
+         
+            Temperature temp2 = new Temperature(temp2Value, temp2Scale);
+            //Convertir Temperatura
+            if (Escala1 != Escala2)
+            {
+                if (Escala1 == "C")
+                {
+                   temp2= temp2.ToCelsius();
+
+                }
+                if (Escala1 == "F")
+                {
+                   temp2= temp2.ToFahrenheit();
+                }
+                if (Escala1 == "K")
+                {
+                    temp2 = temp2.ToKelvin();
+                }
+
+            }
+
+            Console.WriteLine("Ingrese la operación a realizar (+, -, *, /):");
+            string operation = Console.ReadLine();
+            Temperature result = null;
+            switch (operation)
+            {
+                case "+":
+                    result = temp1.Add(temp2);
+                    break;
+                case "-":
+                    result = temp1.Subtract(temp2);
+                    break;
+                case "*":
+                    result = temp1.MultiplyBy(temp2);
+                    break;
+                case "/":
+                    result = temp1.DivideBy(temp2);
+                    break;
+                default:
+                    Console.WriteLine("Operación no válida");
+                    break;
+            }
+            if (result != null)
+                Console.WriteLine("Resultado: " + result);
+            Console.ReadLine();
+        }
+
+    }
+}
---
+++ Temperature.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kata_Temperaturas
+{
+    class Temperature
+    {
+        public enum TemperatureScale { C, F, K }
+
+        private double _value;
+        public double Value { get { return _value; } }
+        public TemperatureScale Scale { get; private set; }
+
+        public Temperature(double value, TemperatureScale scale)
+        {
+            _value = value;
+            Scale = scale;
+        }
+
+        public Temperature Add(Temperature other)
+        {
+            return new Temperature(Value + other.Value, Scale);
+        }
+
+        public Temperature Subtract(Temperature other)
+        {
+            return new Temperature(Value - other.Value, Scale);
+        }
+
+        public Temperature MultiplyBy(Temperature other)
+        {
+            return new Temperature(Value * other.Value, Scale);
+        }
+
+        public Temperature DivideBy(Temperature other)
+        {
+            return new Temperature(Value / other.Value, Scale);
+        }
+
+        public Temperature ToFahrenheit()
+        {
+            if (Scale == TemperatureScale.F)
+                return this;
+            else if (Scale == TemperatureScale.C)
+                return new Temperature((Value * 9 / 5) + 32, TemperatureScale.F);
+            else
+                return new Temperature((Value - 273.15) * 9 / 5 + 32, TemperatureScale.F);
+        }
+
+        public Temperature ToCelsius()
+        {
+            if (Scale == TemperatureScale.C)
+                return this;
+            else if (Scale == TemperatureScale.F)
+                return new Temperature((Value - 32) * 5 / 9, TemperatureScale.C);
+            else
+                return new Temperature(Value - 273.15, TemperatureScale.C);
+        }
+
+        public Temperature ToKelvin()
+        {
+            if (Scale == TemperatureScale.K)
+                return this;
+            else if (Scale == TemperatureScale.F)
+                return new Temperature((Value + 459.67) * 5 / 9, TemperatureScale.K);
+            else
+                return new Temperature(Value + 273.15, TemperatureScale.K);
+        }
+
+        public override string ToString()
+        {
+            string scale = Scale == TemperatureScale.C ? "C" : Scale == TemperatureScale.F ? "F" : "K";
+            return Value + scale;
+        }
+    }
+}
